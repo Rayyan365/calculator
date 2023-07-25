7d6e19b3fc4b4cd2fac46e8694ffe710103bbe22
Rayyan365 <rayanabdallah09@gamil.com>
2023-07-25 00:23:33 -0700

    calcultor done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,23 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculate(numb1, numb2, operator) {
+    if (operator === "+") 
+    {
+      return numb1 + numb2;
+    } else if (operator === "-")
+    {
+        return numb1 - numb2;
+      } else if (operator === "*") 
+    {
+      return numb1 * numb2;
+    } else if (operator === "/") {
+        return numb1 / numb2;
+      }
+      else if (operator === "*") 
+      {
+        return numb1 * numb2;
+      } 
+    } else {
+      return "Invalid operator";
+    }
+  }
